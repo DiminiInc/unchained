407ab24c6257e4e586116296781929503f8d9ba3
Yaskovich Dmitry <kadavr95@outlook.com>
2020-04-04 23:33:10 +0300

    Add settings download and saving

--- scripts/settings.js
+++ scripts/settings.js
@@ -1,8 +1,23 @@
 window.onload = function(){
-document.getElementById('blockedDomains').value = localStorage.getItem('blockedDomains');
-  };
+	inputValue = JSON.parse(localStorage.getItem('blockedDomains'));
+	document.getElementById('blockedDomains').value = inputValue.join("\n");
+	inputValue = JSON.parse(localStorage.getItem('blockedWords'));
+	document.getElementById('blockedWords').value = inputValue.join("\n");
+	inputValue = JSON.parse(localStorage.getItem('partialBlockedDomains'));
+	document.getElementById('partialBlockedDomains').value = inputValue.join("\n");
+	inputValue = JSON.parse(localStorage.getItem('partialAllowedDomains'));
+	document.getElementById('partialAllowedDomains').value = inputValue.join("\n");
+};
 
 
 saveSettings.onclick = function() {
-localStorage.setItem('blockedDomains', document.getElementById('blockedDomains').value);
-  };
+	inputValue = [];
+	inputValue = document.getElementById('blockedDomains').value.split("\n");
+	localStorage.setItem('blockedDomains', JSON.stringify(inputValue));
+	inputValue = document.getElementById('blockedWords').value.split("\n");
+	localStorage.setItem('blockedWords', JSON.stringify(inputValue));
+	inputValue = document.getElementById('partialBlockedDomains').value.split("\n");
+	localStorage.setItem('partialBlockedDomains', JSON.stringify(inputValue));
+	inputValue = document.getElementById('partialAllowedDomains').value.split("\n");
+	localStorage.setItem('partialAllowedDomains', JSON.stringify(inputValue));
+};
